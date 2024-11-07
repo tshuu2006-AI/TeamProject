1c5eefe2dc3bae6134b0824723ebc5cae00b9bc0
Nthuu-AI <tshuu2006@gmail.com>
2024-11-07 21:58:31 +0700

    update

--- Carplaterecogition.py
+++ Carplaterecogition.py
@@ -40,4 +40,17 @@ for i in path:
 df = pd.DataFrame(labels_dict)
 df.to_csv('labels.csv',index=False)
 df.head()
-print(df) 
+
+def getFilename(filename):
+    filename_image = xet.parse(filename).getroot().find('filename').text
+    filepath_image = os.path.join('./Automatic-License-Plate-Detection-main/Automatic-License-Plate-Detection-main/images',filename_image)
+    return filepath_image
+
+image_path = list(df['filepath'].apply(getFilename))
+file_path = image_path[0] #path of our image N137.jpeg
+img = cv2.imread(file_path) #read the image
+# xmin-1804/ymin-1734/xmax-2493/ymax-1882
+img = io.imread(file_path) #Read the image
+fig = px.imshow(img)
+fig.update_layout(width=600, height=500, margin=dict(l=10, r=10, b=10, t=10),xaxis_title='Figure 8 - N137.jpeg with bounding box')
+fig.add_shape(type='rect',x0=401, x1=593, y0=456, y1=493, xref='x', yref='y',line_color='cyan')
