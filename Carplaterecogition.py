68512e3af29c953e064abb3575f7cae2e1259f8e
caoban123 <caoban170106@gmail.com>
2024-11-08 21:33:52 +0700

    "update"

--- Carplaterecogition.py
+++ Carplaterecogition.py
@@ -54,3 +54,4 @@ img = io.imread(file_path) #Read the image
 fig = px.imshow(img)
 fig.update_layout(width=600, height=500, margin=dict(l=10, r=10, b=10, t=10),xaxis_title='Figure 8 - N137.jpeg with bounding box')
 fig.add_shape(type='rect',x0=401, x1=593, y0=456, y1=493, xref='x', yref='y',line_color='cyan')
+#hello
